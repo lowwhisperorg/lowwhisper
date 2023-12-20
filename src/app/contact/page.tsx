7d84e3c3e6377f55ccdaf7ca376c763109fa88c5
Noah Burnette <noah@lowwhisper.org>
2023-12-20 14:46:36 +0000

    another test

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,30 +1,29 @@
-"use client"
 import { Card, CardBody, CardHeader, Input, Button, Link, Radio, RadioGroup } from "@nextui-org/react";
 import React, {useState} from "react";
 
 export default function Contact() {
 
-    const [isSubmitted, setIsSubmitted] = useState(false);
+    // const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
+    // const handleSubmit = (event: React.FormEvent) => {
+    //     event.preventDefault();
 
-        const myForm = event.target as HTMLFormElement;
-        const formData = new FormData(myForm);
-        const formParams = new URLSearchParams()
+    //     const myForm = event.target as HTMLFormElement;
+    //     const formData = new FormData(myForm);
+    //     const formParams = new URLSearchParams()
 
-        formData.forEach((value, key) => {
-            formParams.append(key, value as string);
-        });
+    //     formData.forEach((value, key) => {
+    //         formParams.append(key, value as string);
+    //     });
       
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: formParams.toString(),
-        })
-          .then(() => setIsSubmitted(true))
-          .catch((error) => alert(error));
-    };
+    //     fetch("/", {
+    //       method: "POST",
+    //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //       body: formParams.toString(),
+    //     })
+    //       .then(() => setIsSubmitted(true))
+    //       .catch((error) => alert(error));
+    // };
 
     return (
         <div className="flex flex-col justify-center items-center p-6">
@@ -36,7 +35,7 @@ export default function Contact() {
                     <p>Use the form below to contact me for services or prayer requests!
                         {/* If you would like to donate,please use the <Link href="/donate">donate</Link> page. */}
                     </p>
-                    <form className="p-6" method="POST" name="contact" netlify-data="true" onSubmit={handleSubmit}>
+                    <form className="p-6" method="POST" name="contact" netlify-data="true" action="/success">
                         <input type="hidden" name="form-name" value="contact" />
                         <RadioGroup
                             label="What are you requesting?"
@@ -45,20 +44,20 @@ export default function Contact() {
                             className="py-2"
                             isRequired
                         >
-                            <Radio value="prayer" >Prayer</Radio>
-                            <Radio value="service" >Service</Radio>
-                            <Radio value="other" >Other</Radio>
+                            <Radio value="prayer" id="prayer" >Prayer</Radio>
+                            <Radio value="service" id="service" >Service</Radio>
+                            <Radio value="other" id="other" >Other</Radio>
 
                         </RadioGroup>
-                        <Input isRequired type="text" name="name" label="Name" />
-                        <Input isRequired type="email" name="email" label="Email" />
+                        <Input isRequired type="text" name="name" label="Name" id="name"/>
+                        <Input isRequired type="email" name="email" label="Email" id="email" />
                         <Input
                             label="Phone Number (Optional)"
                             type="telephone"
                             name="telephone"
                         />
 
-                        <Input isRequired type="textarea" label="Message" name="message" />
+                        <Input isRequired type="textarea" label="Message" name="message" id="message" />
                         
 
                         <Button
@@ -70,7 +69,7 @@ export default function Contact() {
                             Send Message
                         </Button>
                     </form>
-                    {isSubmitted && <center><p className="p-6">Thank you! I will get back to you soon.</p></center>}
+                    {/* {isSubmitted && <center><p className="p-6">Thank you! I will get back to you soon.</p></center>} */}
                 </CardBody>
             </Card>
         </div>
